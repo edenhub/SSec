fba55e6619e7eb8c3ce44e9720c59958f0f9b887
edenhub <adamchen.pub@gmail.com>
2014-11-27 16:28:04 +0800

    EasyMock

---
+++ src/main/test/TestMock/Web/AbsStudent.java
@@ -0,0 +1,11 @@
+package TestMock.Web;
+
+/**
+ * Created by adam on 2014/11/27.
+ */
+public abstract class AbsStudent implements IStudent {
+
+//    public abstract String doMethod1();
+//    public abstract String doMethod2();
+    public abstract String doMethod3();
+}
---
+++ src/main/test/TestMock/Web/PartialMocck.java
@@ -0,0 +1,12 @@
+package TestMock.Web;
+
+import org.easymock.EasyMock;
+import org.junit.Test;
+
+/**
+ * Created by adam on 2014/11/27.
+ */
+public class PartialMocck {
+
+
+}
